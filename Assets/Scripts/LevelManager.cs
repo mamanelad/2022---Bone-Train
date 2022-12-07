96a88cbb4036b49b2e4df56a73635b616ce60dc4
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2022-12-08 01:18:39 +0200

    updates level manager

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -23,7 +23,7 @@ public class LevelManager : MonoBehaviour
     private void Start()
     {
         uiEvent.SetActive(false);
-        StartLevel(data);
+        //StartLevel(data);
     }
     
     private void Update()
@@ -66,7 +66,7 @@ public class LevelManager : MonoBehaviour
     {
         if (InLevel)
             return false;
-
+        
         InLevel = true;
         data = levelData;
         interactionIndex = 0;
