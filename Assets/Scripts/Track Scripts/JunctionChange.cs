a93c0badb6f3ae2d9f2f4adfa0135374e4552529
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2022-12-28 20:55:39 +0200

    added junction sphere collider

--- Assets/Scripts/Track Scripts/JunctionChange.cs
+++ Assets/Scripts/Track Scripts/JunctionChange.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class JunctionChange : MonoBehaviour
 {
@@ -9,22 +11,40 @@ public class JunctionChange : MonoBehaviour
     [SerializeField]private BezierSpline rightTrack;
     [SerializeField] private float speed = 4f;
     private SplineWalker train;
-    private Collider collider;
-    
-    void Start()
+    [SerializeField] private Collider colliderSphere;
+    [SerializeField] private Collider colliderBox;
+
+    private void Start()
     {
-        collider = GetComponent<Collider>();
+        colliderSphere.enabled = true;
+        colliderBox.enabled = false;
     }
-    
+
+    private void SwitchColliders()
+    {
+        colliderSphere.enabled = true;
+        colliderBox.enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Train"))
         {
-            var track = Random.Range(1, 3) == 1 ? leftTrack : rightTrack;
-            train = other.GetComponent<SplineWalker>();
-            var trackPosition = transform.transform.position - track.GetControlPoint(0); 
-            train.StartCoroutine(train.SwitchTrack(track, trackPosition, 0.0f, speed));
-            collider.enabled = false;
+            if (colliderSphere.enabled)
+            {
+                print("aaaa");
+                GameManager.Shared.ArrowsTurnOnAndOff(true);
+                SwitchColliders();
+            }
+
+            else if (colliderBox.enabled)
+            {
+                var track = Random.Range(1, 3) == 1 ? leftTrack : rightTrack;
+                train = other.GetComponent<SplineWalker>();
+                var trackPosition = transform.transform.position - track.GetControlPoint(0); 
+                train.StartCoroutine(train.SwitchTrack(track, trackPosition, 0.0f, speed));
+                colliderBox.enabled = false;
+            }
         }
             
     }
