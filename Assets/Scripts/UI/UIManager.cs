1f322b45178a0bcf6d05880c83efae4c4513ea7b
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2023-01-12 06:18:49 +0200

    added item container

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,27 +6,52 @@ using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [Header("Text")]
-    [SerializeField] public TextMeshProUGUI goodSoulsNumberText;
+    [SerializeField] private GameObject itemArray;
+    [SerializeField] private GameObject itemPrefab;
+
+    private Dictionary<string, GameObject> itemDict = new Dictionary<string, GameObject>();
+
+    [Header("Text")] [SerializeField] public TextMeshProUGUI goodSoulsNumberText;
     [SerializeField] public TextMeshProUGUI badSoulsNumberText;
     [SerializeField] public TextMeshProUGUI soulStonesAmountNumberText;
 
-    
 
     public void SetGoodSouls()
     {
-        goodSoulsNumberText.text = Convert.ToString(GameManager.Shared.GoodSouls); 
+        goodSoulsNumberText.text = Convert.ToString(GameManager.Shared.GoodSouls);
     }
-    
-    
-    
+
+
     public void SetBadSouls()
     {
-        badSoulsNumberText.text = Convert.ToString(GameManager.Shared.BadSouls); 
+        badSoulsNumberText.text = Convert.ToString(GameManager.Shared.BadSouls);
     }
-    
+
     public void SetSoulStones()
     {
-        soulStonesAmountNumberText.text = Convert.ToString(GameManager.Shared.SoulStones); 
+        soulStonesAmountNumberText.text = Convert.ToString(GameManager.Shared.SoulStones);
+    }
+
+    public void AddItem(ItemData data)
+    {
+        var newItem = Instantiate(itemPrefab, Vector3.zero, Quaternion.identity, itemArray.transform);
+        newItem.GetComponent<Item>().LoadItemData(data);
+        itemDict[data.name] = newItem;
+    }
+
+    public bool CheckIfPlayerGotItem(ItemData data)
+    {
+        return itemDict.ContainsKey(data.name);
+    }
+
+    public bool RemoveItem(ItemData data)
+    {
+        if (CheckIfPlayerGotItem(data))
+        {
+            Destroy(itemDict[data.name]);
+            return true;
+        }
+
+        return false;
     }
 }
