7da8f8492a3e6c1e7fed782be546712cd11f01a8
tamirsagi891 <93332015+tamirsagi891@users.noreply.github.com>
2023-02-01 17:36:09 +0200

    minor

---
+++ Assets/Scripts/SpecilaEventsHandler.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecilaEventsHandler : MonoBehaviour
+{
+    private struct SpecialEventsData
+    {
+        
+    }
+    
+}
